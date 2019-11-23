62509b4efffb58700de5f364ac6117cd8ecd92ec
hesham-medhat <heshammedhat5@gmail.com>
2019-11-23 09:57:13 +0200

    Initialize GUI elements

--- src/Application/Controller.java
+++ src/Application/Controller.java
@@ -1,4 +1,20 @@
 package Application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class Controller {
+    ObservableList<String> algorithmsList =
+            FXCollections.observableArrayList("DFS", "BFS", "AStar Manhattan", "AStar Euclidean");
+
+    @FXML
+    private ChoiceBox<String> algorithmPicker;
+
+    @FXML
+    private void initialize() {
+        algorithmPicker.setValue("DFS");
+        algorithmPicker.setItems(algorithmsList);
+    }
 }
